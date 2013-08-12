82e51645cd9b65f1fc7d34102357e6c508471a93
petertian <maishu.pro@gmail.com>
2013-08-12 15:48:40 +0800

    delete

--- js/login.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-	$('#loginform input').focus(function(){$(this).addClass('focusinput').removeClass('normalinput');})
-	                     .blur(function(){$(this).addClass('normalinput').removeClass('focusinput');});
-	// $('input').bind('click',function(){ 
-	// 	$('#loginTip,#forgotPWD').show();
-
-	// });
-
-});
